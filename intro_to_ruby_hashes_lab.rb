4eb73495244287823a3cebbcd30fb64ac402de5f
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 15:31:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,12 +51,12 @@ def monopoly_with_fourth_tier
       two_pieces_owned:50,
       one_piece_owned:25
     },
-      names:{
-      reading_railroad: {},
-      pennsylvania_railroad: {},
-      b_and_o_railroad: {},
-      shortline_railroad:{}
-    }
+    names:{
+      reading_railroad: {mortgage_value:"$"+1},
+      pennsylvania_railroad: {mortgage_value:"$"+1},
+      b_and_o_railroad: {mortgage_value:"$"+1},
+      shortline_railroad:{mortgage_value:"$"+1}
     }
   }
+}
 end
