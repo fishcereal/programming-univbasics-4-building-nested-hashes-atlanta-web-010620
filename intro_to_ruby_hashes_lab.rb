d7cacab6fa0e08831e658780e2878dc1c0195b28
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-05 15:26:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
     rent_in_dollars:{
       key2:1,
       key3:1,
-      two_piece_owned:50,
+      two_pieces_owned:50,
       one_piece_owned:25
     },
       names:{
